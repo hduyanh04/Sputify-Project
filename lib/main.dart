84c4e03149935093485ffad7ccd7290289837454
HieuBe1210 <tinhtailor@gmail.com>
2025-07-26 23:27:30 +0700

    [Update] new function

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:sputify/core/config/theme/app_theme.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: AppTheme.lightTheme, home: Container());
+  }
+}
