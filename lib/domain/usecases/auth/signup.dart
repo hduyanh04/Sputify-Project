e0d80214609e88254e15fad29cf9a8cc3dd33603
hduyanh04 <premiumtube3101@gmail.com>
2025-08-01 00:16:59 +0700

    duyanh commit

---
+++ lib/domain/usecases/auth/signup.dart
@@ -0,0 +1,13 @@
+import 'package:sputify/core/usecase/usecase.dart';
+import 'package:sputify/data/models/auth/create_user_req.dart';
+import 'package:dartz/dartz.dart';
+import 'package:sputify/domain/repository/auth/auth_repository_impl.dart';
+import 'package:sputify/service_locator.dart';
+import '../../repository/auth/auth.dart';
+
+class SignupUseCase implements UseCase<Either,CreateUserReq> {
+  @override
+  Future<Either> call({CreateUserReq ? params}) async {
+    return sl<AuthRepository>().signup(params!);
+  }
+}
